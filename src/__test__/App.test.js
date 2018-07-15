7c7e678b873c67ed8c7cc992e73c09b80930fa9e
RA <ra@example.com>
2018-07-16 00:06:46 +0300

    testa

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,9 +1,37 @@
 import 'jsdom-global/register'; 
 
-import App from '../App';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
- shallow(<App />);
-});
+import App from '../App';
+import ListView from '../Components/ListView.js'
+import ExampleItem from '../Components/ExampleItem.js'
+
+describe('<App>', () => {
+
+    it('renders without crashing', () => {
+        shallow(<App />);
+       });
+    
+    it('renders 121 of 1000 exampleItem on 1200 height screen', () => {
+
+        const wrapper = mount(<ListView numRows={1000} rowHeight={10} />);
+        wrapper.setState({availableHeight : 1200})
+
+        expect(wrapper.find(ExampleItem).length).toBe(121);
+  
+    });
+
+    it('renders at first, 121, then 161 of 1000 exampleItem on 1200 height screen', () => {
+
+        const wrapper = mount(<ListView numRows={1000} rowHeight={10} />);
+        wrapper.setState({availableHeight : 1200});
+        wrapper.setState({availableHeight : 1600});
+
+        expect(wrapper.find(ExampleItem).length).toBe(161);
+      
+    });
+
+})
+
+
