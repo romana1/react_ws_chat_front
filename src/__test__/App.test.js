cbc5af370ebada52924f9eb62b0168ac829ad767
RA <ra@example.com>
2018-07-16 13:51:48 +0300

    tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -22,13 +22,31 @@ describe('<App>', () => {
   
     });
 
-    it('renders at first, 121, then 161 of 1000 exampleItem on 1200 height screen', () => {
+    it('renders at first, 121 on 1200 height screen, then 161 of 1000 exampleItem on 1600 height screen', () => {
 
         const wrapper = mount(<ListView numRows={1000} rowHeight={10} />);
         wrapper.setState({availableHeight : 1200});
         wrapper.setState({availableHeight : 1600});
 
         expect(wrapper.find(ExampleItem).length).toBe(161);
+      
+    });
+
+    it('renders following elements on scroll ', () => {
+
+        const wrapper = mount(<ListView numRows={1000} rowHeight={10} />);
+        wrapper.setState({availableHeight : 1200});
+
+        const rootDomNode = wrapper.getDOMNode();
+        rootDomNode.scrollTop = 1300;
+        wrapper.simulate('scroll', {target : rootDomNode});
+        wrapper.update();
+
+        expect(wrapper.find(ExampleItem).at(0).contains(<div className="example">
+        This is
+        130
+      </div>)).toBe(true);
+
       
     });
 
